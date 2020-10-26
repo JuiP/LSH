b6a859b19e46edc5f668c7de74f349b5d7e4d366
Kriti <kriti.jethlia@yahoo.com>
2020-10-26 20:43:19 +0530

    Fixing a bug ;)

--- shingling.py
+++ shingling.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 import pickle
 from collections import defaultdict
+import copy 
 
 def load(doc) :
     file = open(doc,'rb')
@@ -12,19 +13,19 @@ def load(doc) :
 def shingling(data , k) :
     num_of_doc = len(data)
     listofzeros = [0] * num_of_doc
-    shingles_dict=defaultdict(lambda : listofzeros)
+    shingles_dict=defaultdict(lambda : copy.deepcopy(listofzeros))
     for doc in range (0,num_of_doc) :
-        for x in range (0,len(data[doc])-k) :
+        for x in range (0,len(data[doc])-k+1) :
             shingles_dict[data[doc][x:x+k]][doc]=1
-    
+            
     filehandler = open("shingles.obj","wb")
     pickle.dump(dict(shingles_dict),filehandler)
     filehandler.close()
 
 def main():
     data = load('human_data.obj')
-    k = 1000
+    k = 5
     shingling(data , k)
 
 #uncomment to run
-# main()
+main()
