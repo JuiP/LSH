e4481e2969899fe3d0e554d14c13b16a61cea8ed
JuiP <juipradhan2k@gmail.com>
2020-10-27 19:06:19 +0530

    Generate unique functions

--- shingling_minhashing.py
+++ shingling_minhashing.py
@@ -30,31 +30,44 @@ def shingling(data , k) :
     # filehandler.close()
     # return shingles_dict
 
-def genhash(length, num, x):
+def hashfunc(num, length):
+	'''
+	num is the number of hash functions to be created.
+	Returns list of (a,b) pairs.
+	Hash function is of the form (ax+b)modlength
+	Here a and b is always smaller than the number of shingles.
+	'''
+	a = randint(1,length)
+	b = randint(1,length)
+	functions = {(a,b)}
+	while len(functions) < num:
+		a = randint(1,length)
+		b = randint(1,length)
+		functions.update({(a,b)})
+	#print(list(functions))
+	return list(functions)
+
+def genhash(length, num, x, func):
 	'''
 	length is the number of shingles.
-	num is the number of hash functions to be generated.
-	x is the hash function value for the row x
+	num is the number of hash functions.
+	x is the row for which hash function value is to be calculated.
+	func is the list returned by hashfunc()
+	Returns the list containing hash functions value for row x.
 	'''
 	hashes = []
 	for i in range (0, num):
-		def hashfunc(x):
-			'''
-			Returns the hash function value for x.
-			Hash function is of the form (ax+b)modlength
-			Here a and b is always smaller than the number of shingles.
-			'''
-			return(randint(1,length)*x + randint(1,length))%length
-		hashes.append(hashfunc(x))
+		val = (func[i][0] * x + func[i][1]) % length
+		hashes.append(val)
 	return hashes
 
 
-
-def signature_matrix(shingles, num, no_of_doc):
+def signature_matrix(shingles, num, no_of_doc, func):
 	'''
 	shingles is the Input matrix with value of dictionary as the shingles.
 	num is the number of minhash functions to be generated.
 	no_of_doc is the number of documents in data.
+	func is the list returned by hashfunc()
 	'''
 	shingles_list = list(shingles.keys())
 	listofinfinity = [sys.maxsize] * no_of_doc
@@ -66,7 +79,7 @@ def signature_matrix(shingles, num, no_of_doc):
 
 	# row refers to row of input matrix (conceptually)
 	for row in range (0, len(shingles_list)):
-		hashes = genhash(len(shingles_list), num, row)
+		hashes = genhash(len(shingles_list), num, row, func)
 		for col in range (0, no_of_doc):
 			if shingles[shingles_list[row]][col] == 1:
 				for n in range (0, num):
@@ -79,11 +92,28 @@ def main():
     data = load('human_data.obj')
     k = 5
     shingles = shingling(data , k)
-    signature_mat = signature_matrix(shingles, 100, len(data))
-    filehandler = open("signature_matrix.obj","wb")
-    pickle.dump(dict(signature_mat),filehandler)
-    filehandler.close()
+    func = hashfunc(100, len(data))
+    signature_mat = signature_matrix(shingles, 100, len(data), func)
+    
+    # --------------Debugging--------------------------------
+    # shingles = \
+    # { "0": [0,1,1,0],
+    #   "1": [0,0,1,1],
+    #   "2": [1,0,0,0],
+    #   "3": [0,1,0,1],
+    #   "4": [0,0,0,1],
+    #   "5": [1,1,0,0],
+    #   "6": [0,0,1,0]
+    # }
+    # func = hashfunc(3, 4)
+    # signature_mat = signature_matrix(shingles, 3, 4, func)
     # print(signature_mat)
+    # --------------------------------------------------------
+
+    # filehandler = open("signature_matrix.obj","wb")
+    # pickle.dump(dict(signature_mat),filehandler)
+    # filehandler.close()
+    
 
 #uncomment to run
 main()
